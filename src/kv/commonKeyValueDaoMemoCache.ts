7902f32af7d3fc7560609d489eeb40cec2ca6480
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2022-01-29 01:45:16 +0100

    fix: adapt CommonKeyValueDaoMemoCache

--- src/kv/commonKeyValueDaoMemoCache.ts
+++ src/kv/commonKeyValueDaoMemoCache.ts
@@ -1,20 +1,33 @@
 import { AsyncMemoCache } from '@naturalcycles/js-lib'
 import { CommonKeyValueDao } from './commonKeyValueDao'
 
+/**
+ * AsyncMemoCache implementation, backed by CommonKeyValueDao.
+ *
+ * Does NOT support persisting Errors, skips them instead.
+ *
+ * Also, does not support .clear(), as it's more dangerous than useful to actually
+ * clear the whole table/cache.
+ */
 export class CommonKeyValueDaoMemoCache<VALUE = any> implements AsyncMemoCache<string, VALUE> {
   constructor(private dao: CommonKeyValueDao<VALUE>) {}
 
-  async get(k: string): Promise<VALUE | undefined> {
+  async get(k: string): Promise<VALUE | Error | undefined> {
     return (await this.dao.getById(k)) || undefined
   }
 
-  async set(k: string, v: VALUE): Promise<void> {
+  async set(k: string, v: VALUE | Error): Promise<void> {
+    if (v instanceof Error) {
+      // We currently don't persist errors there
+      return
+    }
+
     await this.dao.save(k, v)
   }
 
   async clear(): Promise<void> {
     throw new Error(
-      'CommonKeyValueDaoCacheFactory.clear is not supported, because cache is expected to be persistent',
+      'CommonKeyValueDaoMemoCache.clear is not supported, because cache is expected to be persistent',
     )
   }
 }
